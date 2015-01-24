49c6604e259979f64a5a67764d2057ea52ef6d33
Oliver Richardson <orichardson95@gmail.com>
2015-01-24 15:10:17 -0700

    Add more files

--- desktop/src/com/ggj2015/whatnow/desktop/DesktopLauncher.java
+++ desktop/src/com/ggj2015/whatnow/desktop/DesktopLauncher.java
@@ -5,8 +5,21 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ggj2015.whatnow.WhatNow;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new WhatNow(), config);
+	public static void main(String[] arg) {
+		if (arg.length == 0) {
+			System.out.println("HMM");
+			LwjglApplicationConfiguration config =
+					new LwjglApplicationConfiguration();
+			new LwjglApplication(new WhatNow(), config);
+		}
+		else if (arg[0].startsWith("-E"))
+		{
+			
+			LwjglApplicationConfiguration config =
+					new LwjglApplicationConfiguration();
+			new LwjglApplication(new WhatNow(true), config);
+		
+		}
+
 	}
 }
