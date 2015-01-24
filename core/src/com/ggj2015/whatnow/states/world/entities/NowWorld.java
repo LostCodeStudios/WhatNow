9e746fb68efbcc7f98bfa7b068dd5a3c61b7c38e
MadcowD <carlostnb@gmail.com>
2015-01-24 15:02:49 -0700

    I AM A GOD

--- core/src/com/ggj2015/whatnow/states/world/entities/NowWorld.java
+++ core/src/com/ggj2015/whatnow/states/world/entities/NowWorld.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Json;
 import com.ggj2015.whatnow.states.world.level.Level;
 import com.lostcode.javalib.entities.EntityWorld;
@@ -15,9 +16,6 @@ public class NowWorld extends EntityWorld {
 	
 	public NowWorld(InputMultiplexer input, Camera camera, String levelData) {
 		super(input, camera, Vector2.Zero.cpy());
-		// TODO form proper bounds around map
-		bounds = new Rectangle(-20, -12, 40, 24); // close to screen size
-		
 		buildLevel(levelData);
 		
 	}
@@ -28,8 +26,19 @@ public class NowWorld extends EntityWorld {
 		Level level = jsonParser.fromJson(Level.class, Gdx.files.internal(levelDataFile));
 		
 		//Establish basic world charactersitics.
-		this.camera.position.set(level.getCameraInitial());
-		this.getPhysicsWorld();
+		this.camera.position.set(new Vector3(level.getCameraInitial(),0));
+		this.physicsWorld.setGravity(level.getGravity());
+		this.bounds = level.getBounds();
+		this.spriteSheet = level.getSpriteSheet();
+		
+		//Initialize the game objects or entities :)
+//		for(GameObject obj : level.getGameObjects()){
+//			
+//		}
+		
+		
+		
+		
 	}
 	
 	@Override
