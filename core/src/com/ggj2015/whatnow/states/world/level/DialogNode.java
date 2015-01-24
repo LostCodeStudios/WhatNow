6af976fb02ebae59663600be4d1737ad46fa67f2
unknown <u0847773@FA-EAE-188.ad.utah.edu>
2015-01-24 12:11:38 -0700

    Level editor stuff

--- core/src/com/ggj2015/whatnow/states/world/level/DialogNode.java
+++ core/src/com/ggj2015/whatnow/states/world/level/DialogNode.java
@@ -1,11 +1,13 @@
 package com.ggj2015.whatnow.states.world.level;
 
 import com.badlogic.gdx.utils.Array;
+import com.sun.istack.internal.Nullable;
 
 public class DialogNode {
-	Array<String> text;
-	Array<String> options;
-	Array<DialogNode> next; 
+	public Array<String> text;
+	public Array<String> options;
+	@Nullable
+	public Array<DialogNode> next; 
 	// these can be null, and are in 1-1 corespondance
 	// to the "options" array. 
 }
---
+++ core/src/com/ggj2015/whatnow/states/world/level/Level.java
@@ -0,0 +1,122 @@
+/**
+ * 
+ */
+package com.ggj2015.whatnow.states.world.level;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.lostcode.javalib.utils.SpriteSheet;
+
+/**
+ * @author u0847773
+ * The level class
+ */
+public class Level {
+	//FIELDS
+	String name;
+	Rectangle bounds;
+	Vector2 gravity;
+	String spriteSheetFile;
+	Vector2 cameraInitial;
+	SpriteSheet spriteSheet;
+	ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
+	/**
+	 * @param name The name of the level 
+	 * @param bounds The bounds of the level.
+	 * @param gravity The world gravity -_-, god knows why
+	 * @param spriteSheet The path to the sprite sheet XML file.
+	 * @param cameraInitial initial position of the camera.
+	 */
+	public Level(String name, Rectangle bounds, Vector2 gravity,
+			String spriteSheet, Vector2 cameraInitial) {
+		super();
+		this.name = name;
+		this.bounds = bounds;
+		this.gravity = gravity;
+		setSpriteSheet(spriteSheet);
+		this.cameraInitial = cameraInitial;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the gameObjects
+	 */
+	public ArrayList<GameObject> getGameObjects() {
+		return gameObjects;
+	}
+	/**
+	 * @return the bounds
+	 */
+	public Rectangle getBounds() {
+		return bounds;
+	}
+	/**
+	 * @param bounds the bounds to set
+	 */
+	public void setBounds(Rectangle bounds) {
+		this.bounds = bounds;
+	}
+	/**
+	 * @return the gravity
+	 */
+	public Vector2 getGravity() {
+		return gravity;
+	}
+	/**
+	 * @param gravity the gravity to set
+	 */
+	public void setGravity(Vector2 gravity) {
+		this.gravity = gravity;
+	}
+	/**
+	 * @return the cameraInitial
+	 */
+	public Vector2 getCameraInitial() {
+		return cameraInitial;
+	}
+	/**
+	 * @param cameraInitial the cameraInitial to set
+	 */
+	public void setCameraInitial(Vector2 cameraInitial) {
+		this.cameraInitial = cameraInitial;
+	}
+	/**
+	 * @return the spriteSheet
+	 */
+	public SpriteSheet getSpriteSheet() {
+		return spriteSheet;
+	}
+	/**
+	 * @param spriteSheet the spriteSheet to set
+	 */
+	public void setSpriteSheet(String spriteSheet) {
+		this.spriteSheetFile = spriteSheet;
+		try {
+			this.spriteSheet = SpriteSheet.fromXML(Gdx.files.internal(spriteSheet));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String getSpriteSheetFile(){
+		return this.spriteSheetFile;
+	}
+	
+	
+	
+}
