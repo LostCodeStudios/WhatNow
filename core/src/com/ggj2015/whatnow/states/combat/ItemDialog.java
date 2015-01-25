d8b4618abc0a1e971c914f28691e74e129538e30
Nathaniel Nelson <nelson.nleroy@gmail.com>
2015-01-24 17:33:25 -0700

    Added a tile sprite

--- core/src/com/ggj2015/whatnow/states/combat/ItemDialog.java
+++ core/src/com/ggj2015/whatnow/states/combat/ItemDialog.java
@@ -1,7 +1,9 @@
 package com.ggj2015.whatnow.states.combat;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ObjectMap;
 import com.ggj2015.whatnow.states.DialogMenu;
 import com.ggj2015.whatnow.states.DialogStyle;
 import com.ggj2015.whatnow.states.combat.CombatScreen.CombatState;
@@ -9,16 +11,31 @@ import com.ggj2015.whatnow.states.world.level.DialogNode;
 
 public class ItemDialog extends DialogMenu {
 
+	static class ItemData {
+		static enum EffectType {
+			Heal,
+			Damage
+		}
+		
+		public String name = "";
+		public String description = "";
+		public EffectType type = EffectType.Heal;
+		public int minEffect = 0;
+		public int maxEffect = 0;
+	}
+	
 	CombatScreen screen;
 	
 	private static final Array<String> text = new Array<String>();
 	private static final Array<String> options = new Array<String>();
 	private static final Array<Boolean> optionsEnabled = new Array<Boolean>();
 	
+	private static final ObjectMap<String, ItemData> items = new ObjectMap<String, ItemData>();
+	
 	private static final DialogStyle style = DialogStyle.DEFAULT;
 	
 	static {
-		options.add("Throw rock");
+		String itemFile = Gdx.files.internal("combat/items.txt").readString();
 		
 		for (int i = 0; i < options.size; i++) {
 			optionsEnabled.add(true);
