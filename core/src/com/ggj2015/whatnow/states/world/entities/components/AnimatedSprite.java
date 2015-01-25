fc17ec677ae5e4bcf6fb68625c936ff3c14dcf83
Oliver Richardson <orichardson95@gmail.com>
2015-01-25 12:37:28 -0700

    Animated sprites! They work :)

--- core/src/com/ggj2015/whatnow/states/world/entities/components/AnimatedSprite.java
+++ core/src/com/ggj2015/whatnow/states/world/entities/components/AnimatedSprite.java
@@ -1,5 +1,6 @@
 package com.ggj2015.whatnow.states.world.entities.components;
 
+import com.badlogic.gdx.utils.Array;
 import com.ggj2015.whatnow.states.world.entities.NowWorld;
 import com.lostcode.javalib.entities.components.render.MultiRenderable;
 import com.lostcode.javalib.entities.components.render.Renderable;
@@ -23,21 +24,34 @@ public class AnimatedSprite extends MultiRenderable {
 	public static AnimatedSprite newSprite(NowWorld world, String feetPrefix,
 			String handPrefix, String bodyKey) {
 		String feetKeyStub = feetPrefix;
+		if(!feetPrefix.isEmpty())
+			feetKeyStub += "-";
 		feetKeyStub += "feet"; // now requires frame #
 
 		String handKeyStub = handPrefix;
+		if(!handPrefix.isEmpty())
+			handKeyStub += "-";
 		handKeyStub += "hands";
 
+		
 		Sprite body = new Sprite(world.getSpriteSheet(), bodyKey);
 
-		AnimatedSprite as = new AnimatedSprite(body); // TODO
+		AnimatedSprite as = new AnimatedSprite(body); // TODO'
+		
 
 		as.lHandSprite = new Sprite(world.getSpriteSheet(), handKeyStub + "L");
 		as.rHandSprite = new Sprite(world.getSpriteSheet(), handKeyStub + "R");
 
 		as.lFootSprite = new Sprite(world.getSpriteSheet(), feetKeyStub + "L");
 		as.rFootSprite = new Sprite(world.getSpriteSheet(), feetKeyStub + "R");
-		
+
+		as.addChild(as.lHandSprite);
+		as.addChild(as.rHandSprite);
+		as.addChild(as.lFootSprite);
+		as.addChild(as.rFootSprite);
+
+		as.reorder(new Array<Integer>(new Integer[] { 3, 4, 1, 2, 0 }));
+
 		return as;
 	}
 
--- core/src/com/ggj2015/whatnow/states/world/entities/systems/AnimateSpriteSystem.java
+++ core/src/com/ggj2015/whatnow/states/world/entities/systems/AnimateSpriteSystem.java
@@ -1,6 +1,6 @@
 package com.ggj2015.whatnow.states.world.entities.systems;
 
-import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.math.Vector2;
 import com.ggj2015.whatnow.states.world.entities.components.AnimatedSprite;
 import com.lostcode.javalib.entities.Entity;
 import com.lostcode.javalib.entities.components.physical.Body;
@@ -26,15 +26,46 @@ public class AnimateSpriteSystem extends ComponentSystem {
 		Body b = (Body) e.getComponent(Body.class);
 
 		as.animating = b.getLinearVelocity().len() > 0.05f;
+		float speed = b.getLinearVelocity().len() * 1.2f;
+		float h2 = as.getBase().getHeight() / 2, offset = 30f;
 
 		if (as.animating) {
 			as.elapsedSec += deltaSeconds();
+
+			float s1 =
+					(float) (Math.sin(as.elapsedSec * speed)
+							* as.getBase().getWidth() / 3);
+			float s2 =
+					(float) (Math.sin((as.elapsedSec * speed) + Math.PI)
+							* as.getBase().getWidth() / 3);
+			float s3 =
+					(float) (Math.sin((as.elapsedSec * speed) + Math.PI + 0.5)
+							* as.getBase().getWidth() / 2);
+			float s4 =
+					(float) (Math.sin((as.elapsedSec * speed) - 0.5)
+							* as.getBase().getWidth() / 2);
+
+			as.lFootSprite.setOrigin(new Vector2(s1 + offset, -h2 / 2));
+			as.rFootSprite.setOrigin(new Vector2(s2 + offset, h2 / 2));
+			as.lHandSprite.setOrigin(new Vector2(s3, -h2));
+			as.rHandSprite.setOrigin(new Vector2(s4, h2));
+		}
+		else {
+			as.lFootSprite.setOrigin(new Vector2(+offset, -h2 / 2));
+			as.rFootSprite.setOrigin(new Vector2(+offset, h2 / 2));
+			as.lHandSprite.setOrigin(new Vector2(0, -h2));
+			as.rHandSprite.setOrigin(new Vector2(0, h2));
 		}
 
-		as.reorder(new Array<Integer>(new Integer[] { 1, 2, 0 }));
+		as.rotationTo = (float) Math.toRadians(b.getLinearVelocity().angle());
 
-		as.setRotation((float) Math.toDegrees(as.rotation));
-		as.rotation += (as.rotationTo - as.rotation) * 0.1f;
+		while (as.rotationTo - as.rotation > Math.PI)
+			as.rotationTo -= 2 * Math.PI;
+		while (as.rotation - as.rotationTo > Math.PI)
+			as.rotationTo += 2 * Math.PI;
+
+		as.rotation += (as.rotationTo - as.rotation) * 0.3f;
+		System.out.println(as.rotation);
+		b.setRotation(as.rotation);
 	}
-
 }
