ed701dd70aec026e7d70a0a5dbb613bd9d004e4e
MadcowD <carlostnb@gmail.com>
2015-01-25 00:51:01 -0700

    Beginnings of the props

---
+++ core/src/com/ggj2015/whatnow/states/world/entities/systems/PropActivationSystem.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.ggj2015.whatnow.states.world.entities.systems;
+
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputMultiplexer;
+import com.lostcode.javalib.entities.Entity;
+import com.lostcode.javalib.entities.components.physical.Sensor;
+import com.lostcode.javalib.entities.systems.InputSystem;
+
+/**
+ * @author MadcowD
+ *
+ */
+public class PropActivationSystem extends InputSystem {
+
+	/**
+	 * @param input
+	 */
+	public PropActivationSystem(InputMultiplexer input) {
+		super(input);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public boolean canProcess(Entity e) {
+		if(e.getGroup().equalsIgnoreCase("Props"))
+			return true;
+		else
+			return false;
+	}
+	
+	@Override
+	public boolean keyDown(int keycode) {
+		if(keycode == Keys.SPACE){
+			for(int i = 0; i < this.entities.size; i++)
+			{
+				Entity e = entities.get(i);
+				if(e != null){
+					Sensor s = (Sensor) e.getComponent(Sensor.class);
+					if(s.contains(e))
+						 //TODO: ACTIVATE CUNT
+						return false;
+				}
+			}
+		}
+		return false;
+	}
+
+}
